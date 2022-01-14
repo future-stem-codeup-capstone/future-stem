0f5f49936e69fc2df3b4c0967b15239681416d10
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-01-14 14:27:42 -0600

    starting future stem

---
+++ src/main/java/com/example/futurestem/FutureStemApplication.java
@@ -0,0 +1,13 @@
+package com.example.futurestem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FutureStemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FutureStemApplication.class, args);
+    }
+
+}
