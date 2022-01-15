d29be10e7bc05f056557367479f2b5ae3bae9ff3
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-01-15 16:59:02 -0600

    fixing user class git issues

--- src/main/java/com/example/futurestem/Models/User.java
+++ src/main/java/com/example/futurestem/Models/User.java
@@ -1,9 +1,7 @@
 package com.example.futurestem.Models;
-
+import java.util.List;
 import javax.persistence.*;
 import java.util.List;
-//import javax.persistence.Table;
-
 
 @Entity
 @Table(name = "users")
@@ -22,5 +20,50 @@ public class User {
     private String password;
 
     @OneToMany(mappedBy = "user")
+
     private List<Project> userProjects;
+
+    public User() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public List<Project> getUserProjects() {
+        return userProjects;
+    }
+
+    public void setUserProjects(List<Project> userPosts) {
+        this.userProjects = userPosts;
+    }
+
+
 }
