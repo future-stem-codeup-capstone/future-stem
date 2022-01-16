e1dbd1d17b0b4e4d2ffac4400a418b54ed9b980f
Ian McConihay <mcconihay.ian92@gmail.com>
2022-01-16 09:15:03 -0600

    Worked on controllers

--- src/main/java/com/example/futurestem/Controllers/UserController.java
+++ src/main/java/com/example/futurestem/Controllers/UserController.java
@@ -1,4 +1,10 @@
 package com.example.futurestem.Controllers;
 
+import com.example.futurestem.Repository.UserRepository;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class UserController {
+	private UserRepository userDao;
+
 }
