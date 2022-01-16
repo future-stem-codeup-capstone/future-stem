2c37de5f1b7168d45adf53d4e89f02b4d56140c0
patric noriega <patricnoriega@gmail.com>
2022-01-16 15:36:40 -0600

    hobbies controller/repository/html

--- src/main/java/com/example/futurestem/Controllers/HobbyController.java
+++ src/main/java/com/example/futurestem/Controllers/HobbyController.java
@@ -2,11 +2,15 @@ package com.example.futurestem.Controllers;
 
 import com.example.futurestem.Models.Hobby;
 import com.example.futurestem.Models.Project;
+import com.example.futurestem.Models.User;
 import com.example.futurestem.Repository.HobbyRepository;
 import com.example.futurestem.Repository.UserRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class HobbyController {
@@ -24,9 +28,27 @@ public class HobbyController {
         return "views/Hobby/create";
     }
 
-    @GetMapping("/views/createHobbies")    //not sure if path needs to be /views/hobby/createHobbies //
+    @GetMapping("/views/createHobbies")
     public String hobbiesCreateForm(Model model) {
         model.addAttribute("hobbies", new Hobby());
         return "views/Hobby/create";
+    }
+
+    @PostMapping("/views/createHobbies")
+    public String createHobby(@ModelAttribute Hobby hobby) {
+
+        User user = userDao.getById(1L);
+        hobby.setUser(user);
+        hobbyDao.save(hobby);
+
+        return "redirect:/views/profile";
+    }
+
+
+    @PostMapping("/hobbies/delete/{id}")
+    public String deletePost(@PathVariable long id) {
+        hobbyDao.deleteById(id);
+
+        return "redirect:/views/profile";
     }
 }
