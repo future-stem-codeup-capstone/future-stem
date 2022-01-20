585f4b8946cc79c6ca838639e582d6ef35d6747c
patric noriega <patricnoriega@gmail.com>
2022-01-20 13:35:54 -0600

    filestack submit

--- src/main/resources/templates/views/index.js
+++ src/main/resources/templates/views/index.js
@@ -1,11 +1,46 @@
-
-const client = filestack.init('AQ87ePcflS1GG7yCbmaMoz');
-const options = {
-    onUploadDone: updateForm,
-    accept: ["image/*"],
+const getUserId = () => {
+    return new Promise((resolve) => {
+        resolve({
+            userId: 1
+        })
+    });
 };
 
-const picker = client.picker(options);
+//  function for saving user data
+const saveUserData = (data) => {
+    return new Promise((resolve) => {
+        console.log(data);
+        resolve({
+            success: true
+        })
+    });
+};
+window.addEventListener('DOMContentLoaded', function () {
+    getUserId().then((response) => {
+        const userId = response.userId;
+        const apikey = 'AQ87ePcflS1GG7yCbmaMoz';
+        const client = filestack.init(apikey);
+        const options = {
+            onUploadDone: updateForm,
+            accept: ["image/*"],
+            uploadInBackground: false,
+            onFileUploadFinished: (response) => {
+                saveUserData({
+                    userId,
+                    fileHandle: response.handle
+                }).then((response) => {
+                    console.log('User data has been saved', response);
+                })
+            }
+        };
+        const picker = client.picker(options);
+// const client = filestack.init('AQ87ePcflS1GG7yCbmaMoz');
+// const options = {
+//     onUploadDone: updateForm,
+//     accept: ["image/*"],
+// };
+//
+// const picker = client.picker(options);
 
 // const client = filestack.init('AQ87ePcflS1GG7yCbmaMoz');
 // const options = {
@@ -17,24 +52,23 @@ const picker = client.picker(options);
 // const picker = client.picker(options);
 
 // references to the DOM elements
-const form = document.getElementById('pick-form');
-const fileInput = document.getElementById('fileupload');
-const btn = document.getElementById('picker');
-const nameBox = document.getElementById('nameBox');
-const urlBox = document.getElementById('urlBox');
+        const form = document.getElementById('pick-form');
+        const fileInput = document.getElementById('fileupload');
+        const btn = document.getElementById('picker');
+        const nameBox = document.getElementById('nameBox');
+        const urlBox = document.getElementById('urlBox');
 
 // our event listeners
-btn.addEventListener('click', function (event) {
-    event.preventDefault();
-    picker.open();
-});
+        btn.addEventListener('click', function (event) {
+            event.preventDefault();
+            picker.open();
+        });
 
-form.addEventListener('submit', function (event) {
-    event.preventDefault();
-    alert('Submitting: ' + fileInput.value);
-});
+        form.addEventListener('submit', function (event) {
+            event.preventDefault();
+            alert('Submitting: ' + fileInput.value);
+        });
 // overwrite the field input value
-
 // function updateForm(result) {
 //     const fileData = result.filesUploaded[0];
 //     fileInput.value = fileData.url;
@@ -47,23 +81,25 @@ form.addEventListener('submit', function (event) {
 //     nameBox.appendChild(name);
 //     urlBox.appendChild(document.createTextNode('Uploaded to: '));
 //     urlBox.appendChild(url);
-function updateForm (result) {
-    const fileData = result.filesUploaded[0];
-    fileInput.value = fileData.url;
+        function updateForm(result) {
+            const fileData = result.filesUploaded[0];
+            fileInput.value = fileData.url;
 
-    // thumbnail preview
-    if (['jpeg', 'png'].indexOf(fileData.mimetype.split('/')[1]) !== -1) {
-        const container = document.getElementById('thumbnail-container');
-        const thumbnail = document.getElementById('thumbnail') || new Image();
-        thumbnail.id = 'thumbnail';
-        thumbnail.src = client.transform(fileData.handle, {
-            resize: {
-                width: 200
+            // thumbnail preview
+            if (['jpeg', 'png'].indexOf(fileData.mimetype.split('/')[1]) !== -1) {
+                const container = document.getElementById('thumbnail-container');
+                const thumbnail = document.getElementById('thumbnail') || new Image();
+                thumbnail.id = 'thumbnail';
+                thumbnail.src = client.transform(fileData.handle, {
+                    resize: {
+                        width: 200
+                    }
+                });
+
+                if (!container.contains(thumbnail)) {
+                    container.appendChild(thumbnail);
+                }
             }
-        });
-
-        if (!container.contains(thumbnail)) {
-            container.appendChild(thumbnail);
         }
-    }
-}
+    });
+});
