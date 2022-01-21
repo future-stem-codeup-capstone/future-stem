a918b3e367dad10078a0f0287711cd68622faf9b
Ian McConihay <mcconihay.ian92@gmail.com>
2022-01-21 14:46:13 -0600

    Connections set up repo

--- src/main/java/com/example/futurestem/Controllers/ConnectionsController.java
+++ src/main/java/com/example/futurestem/Controllers/ConnectionsController.java
@@ -1,4 +1,20 @@
 package com.example.futurestem.Controllers;
 
+import com.example.futurestem.Repository.ConnectionsRepository;
+import com.example.futurestem.Repository.UserRepository;
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class ConnectionsController {
+	private UserRepository userDao;
+	private ConnectionsRepository connectionsDao;
+
+	public ConnectionsController(UserRepository userDao, ConnectionsRepository connectionsDao) {
+		this.userDao = userDao;
+		this.connectionsDao = connectionsDao;
+	}
+
+
+
+
 }
---
+++ src/main/java/com/example/futurestem/Repository/ConnectionsRepository.java
@@ -0,0 +1,6 @@
+package com.example.futurestem.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ConnectionsRepository extends JpaRepository {
+}
