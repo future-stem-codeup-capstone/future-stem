469f81b97ebf6f8acfe8301e78d00ed18683650e
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-01-27 08:28:54 -0600

    added pop ups for places

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -51,6 +51,18 @@ map.once('load', () => {
 		}
 	})
 	
+	map.on('click', 'places-text', (e) => {
+		const places = e.features[0];
+		console.log(places);
+		
+		new mapboxgl.Popup()
+			.setHTML(`<b>${places.properties.PlaceName}</b><br>${places.properties.Place_addr}`)
+			.setLngLat(places.geometry.coordinates)
+			.addTo(map);
+	})
+	
+	map.on('mouseenter')
+	
 	showPlaces();
 	
 	
@@ -94,7 +106,7 @@ function geocode(search, token) {
 
 
 // document.getElementById('places-select').addEventListener('change', showPlaces);
-$('#places-select').change((event) => {showPlaces()});
+$('#places-select').change(() => {showPlaces()});
 $('#location-button').click((e) => {
 	e.preventDefault();
 	geocode($('#location-input').val(), mapboxKey)
