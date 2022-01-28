8c2875094545b628cdd0a7fb2cc2fd7094d27961
patric noriega <patricnoriega@gmail.com>
2022-01-28 14:57:49 -0600

    edit project

--- src/main/java/com/example/futurestem/Models/Project.java
+++ src/main/java/com/example/futurestem/Models/Project.java
@@ -8,7 +8,7 @@ public class Project {
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
+	private long id;
 
 	@Column(nullable = false, length = 100)
 	private String title;
