52df8f52fec373b4af895abc02cc22d5dfadce56
Jackie Dallas <jackie.r.dallas@gmail.com>
2022-01-28 22:43:52 -0600

    added error pages

--- src/main/java/com/example/futurestem/Controllers/StemController.java
+++ src/main/java/com/example/futurestem/Controllers/StemController.java
@@ -2,6 +2,7 @@ package com.example.futurestem.Controllers;
 import com.example.futurestem.Models.User;
 import com.example.futurestem.Repository.HobbyRepository;
 import com.example.futurestem.Repository.ProjectRepository;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
@@ -11,6 +12,8 @@ public class StemController {
     private final HobbyRepository hobbyDao;
     private final ProjectRepository projectDao;
     public StemController(ProjectRepository projectDao, HobbyRepository hobbyDao) {this.projectDao = projectDao; this.hobbyDao = hobbyDao;}
+
+
 
     @GetMapping("/")
     public String showLandingPage(Model model) {
@@ -31,19 +34,16 @@ public class StemController {
     }
 
 
-    @GetMapping("/error")
-    public String showError() {
 
-
-        return "views/error";
-
-    }
 
     @GetMapping("/requests")
     public String showRequests() {return "views/friend-requests";}
 
     @GetMapping("/map")
-    public String showMap() {return "views/map";}
+    public String showMap(Model model) {
+//        model.addAttribute("yelpAPI", apiKey);
+        return "views/map";
+    }
 
 
 }
