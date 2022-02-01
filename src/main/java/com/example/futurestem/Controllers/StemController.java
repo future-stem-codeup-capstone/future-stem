06a96dad9aebb95b02a98a03e6c8e1cf4c742018
Ian McConihay <mcconihay.ian92@gmail.com>
2022-02-01 11:07:46 -0600

    Connectioins show projects, removed, search bar, drop down. added filestack for avatars. Got the dictinary to work.

--- src/main/java/com/example/futurestem/Controllers/StemController.java
+++ src/main/java/com/example/futurestem/Controllers/StemController.java
@@ -1,20 +1,27 @@
 package com.example.futurestem.Controllers;
+import com.example.futurestem.Models.Connections;
+import com.example.futurestem.Models.Project;
 import com.example.futurestem.Models.User;
+import com.example.futurestem.Repository.ConnectionsRepository;
 import com.example.futurestem.Repository.HobbyRepository;
 import com.example.futurestem.Repository.ProjectRepository;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.stereotype.Controller;
+
+import java.util.List;
 
 @Controller
 public class StemController {
     private final HobbyRepository hobbyDao;
     private final ProjectRepository projectDao;
+    private final ConnectionsRepository connectionsDao;
 
-    public StemController(ProjectRepository projectDao, HobbyRepository hobbyDao) {
+    public StemController(ProjectRepository projectDao, HobbyRepository hobbyDao, ConnectionsRepository connectionsDao) {
         this.projectDao = projectDao;
         this.hobbyDao = hobbyDao;
+        this.connectionsDao = connectionsDao;
     }
 
 
@@ -32,6 +39,9 @@ public class StemController {
 
     @GetMapping("/home")
     public String showHome(Model model) {
+        User currentUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+
         model.addAttribute("project", projectDao.findAll());
         model.addAttribute("hobby", hobbyDao.findAll());
         return "views/home";
@@ -54,6 +64,7 @@ public class StemController {
     public String aboutUs(Model model) {
         return "views/about-us";
 
+    }
     @GetMapping("/education")
     public String showEducation(Model model) {
 //        model.addAttribute("yelpAPI", apiKey);
